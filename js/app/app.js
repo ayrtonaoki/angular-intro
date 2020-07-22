623cd074506c3727b213d391c60d8cfd226a0f86
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-07-22 19:59:29 -0300

    Move module to app.js file

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,14 @@
-angular.module('myModule', [])
+angular.module('myModule', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/contact', {
+        templateUrl: 'templates/contact.html',
+        controller: 'indexController'
+      })
+      .when('/home', {
+        templateUrl: 'templates/home.html',
+        controller: 'homeController'
+      })
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+  })
